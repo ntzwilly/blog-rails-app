35f323140e1b2d6a99d0958bf43f80c02468ee19
ntzwilly <ntzwilly@gmail.com>
2022-01-17 12:36:52 +0200

    Create method update_posts_counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   has_many :comments dependent: :destroy
   has_many :likes dependent: :destroy
   belongs_to :user 
+
+  def update_posts_counter(value)
+    user.update('posts_counter', value)
+  end
+
+  def most_recent_comments()
+    comments.last(5)
+  end
 end
