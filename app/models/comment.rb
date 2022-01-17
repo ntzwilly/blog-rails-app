faac982164e05607008a3bf2653c21c7b0fa9ae8
ntzwilly <ntzwilly@gmail.com>
2022-01-17 18:18:49 +0200

    Make required changes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,10 @@
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  after_save :update_comments_counter
+
+  private
 
   def update_comments_counter(value)
     post.update('comments_counter', value)
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,10 @@
 class Like < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  after_save :update_likes_counter
+
+  private
 
   def update_likes_counter(value)
     post.update('likes_counter', value)
