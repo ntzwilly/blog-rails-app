b4be6b336d87b83355a9ed874d6ac066f0793d1f
ntzwilly <ntzwilly@gmail.com>
2022-01-17 12:35:57 +0200

    Create method most_recent_posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def most_recent_posts(limit = 3)
+    posts.last(limit)
+  end
 end
