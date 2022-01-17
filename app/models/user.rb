5bcc1662f706b17a2f2682d93c97d86e2d4a9784
ntzwilly <ntzwilly@gmail.com>
2022-01-17 18:40:18 +0200

    Make required changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts, dependent: :destroy
-  has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
   def most_recent_posts(limit = 3)
     posts.last(limit)
