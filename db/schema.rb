cdd3eff3fbfc679bad8be0346ef9b57d1421fc00
ntzwilly <ntzwilly@gmail.com>
2022-01-19 17:58:07 +0200

    Update the database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_113634) do
+ActiveRecord::Schema.define(version: 2022_01_19_120900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,12 @@ ActiveRecord::Schema.define(version: 2022_01_13_113634) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "posts_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["user_id", "post_id"], name: "index_posts_users_on_user_id_and_post_id"
   end
 
   create_table "users", force: :cascade do |t|
