9e8f0c08ba3459b73fa116ae7d9cabc327a7bfac
ntzwilly <ntzwilly@gmail.com>
2022-01-19 18:11:31 +0200

    Update the like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,7 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  belongs_to :post
+  belongs_to :user
+  belongs_to :post, counter_cache: :likes_counter
 
-  after_save :update_likes_counter
-
-  private
 
   def update_likes_counter(value)
     post.update('likes_counter', value)
