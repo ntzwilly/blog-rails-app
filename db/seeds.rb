d8f4a4ec4e2453cc617a0eb51914ea45f8475308
ntzwilly <ntzwilly@gmail.com>
2022-01-19 17:57:16 +0200

    Add default values for the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+(1..5).each do |i|
+	user = User.create(name: "User #{i}", bio: "Lorem Ipsum Dolor Amet,...")
+	 (1..3).each do |j|
+		post = Post.create(title: "Amazing things to try right now ##{j}", text: "Lorem Ipsum Dolor Amet", user: user)
+		(1..5).each do |j|
+			Comment.create(text: "Comment #{j}", post: post, user: user)
+		end
+	end
+end
+
+  
