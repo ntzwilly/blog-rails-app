3d96af4dec778e22ceb6fb35b03fca4e5125a4f6
ntzwilly <ntzwilly@gmail.com>
2022-01-21 13:28:31 +0200

    Add validations for Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post, counter_cache: :comments_counter
 
-  def update_comments_counter(value)
+  def update_counter(value)
     post.update('comments_counter', value)
   end
 end
