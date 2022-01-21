be3f7aa9d8076d21dbe527a5a8a7bb9a6a809a48
ntzwilly <ntzwilly@gmail.com>
2022-01-21 13:27:53 +0200

    Add validations for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,12 @@
 class Post < ApplicationRecord
+
+  validates :title, presence: true, 
+                    length: { maximum: 250, too_long: 'Title length must not exceed 250 characters' }
+  validates :comments_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+
   belongs_to :user, counter_cache: :posts_counter
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-
-  validates :title presence: true,  length: { maximum: 250, too_long: 'Title length must not exceed 250 characters' }
 
   def most_recent_comments
     comments.last(5)
