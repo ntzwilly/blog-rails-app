7ed376a5186a54792ee93cc8e6554d74f7eab8e6
ntzwilly <ntzwilly@gmail.com>
2022-01-21 10:09:29 +0200

    Add unit specs for User model

--- spec/controllers/users_spec.rb
+++ spec/controllers/users_spec.rb
@@ -1,35 +1,11 @@
+require 'spec_helper'
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET #index' do
-    before(:example) { get users_path }
-
-    it 'should provide correct response status' do
-      expect(response).to have_http_status(:ok)
-    end
-
-    it 'should provide correct template' do
-      expect(response).to render_template(:index)
-    end
-
-    it 'should have correct placeholder text' do
-      expect(response.body).to include('This is where goes all users')
-    end
+RSpec.describe 'routes for users', type: :routing do
+  it 'route of users' do
+    expect(get: '/').to route_to(controller: 'users', action: 'index')
   end
-
-  describe 'GET #show' do
-    before(:example) { get user_path(1) }
-
-    it 'should provide correct response status' do
-      expect(response).to have_http_status(:ok)
-    end
-
-    it 'should provide correct template' do
-      expect(response).to render_template(:show)
-    end
-
-    it 'should have correct placeholder text' do
-      expect(response.body).to include('This is where goes users bio and their posts')
-    end
+  it 'route of user profile' do
+    expect(get: '/users/4').to route_to(controller: 'users', action: 'show', id: '4')
   end
 end
