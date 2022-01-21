a22d1732c9b2c02dec98595839af6efd5e0af75b
ntzwilly <ntzwilly@gmail.com>
2022-01-21 13:28:10 +0200

    Add validations for Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post, counter_cache: :likes_counter
 
-  def update_likes_counter(value)
+  def update_counter(value)
     post.update('likes_counter', value)
   end
 end
