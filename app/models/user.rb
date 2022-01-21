811e394d7ecfce635fbc42febd2400c2bd8d23e2
ntzwilly <ntzwilly@gmail.com>
2022-01-21 10:08:19 +0200

    Add validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0}
+
   def most_recent_posts
-    posts.order('created_at Asc').limit(3)
+    posts.order('created_at Asc').last(3)
   end
 end
